bd1fa8989fd67148893875df9db2629610c5e4d5
poisonshade <imildositoe@gmail.com>
2024-03-13 19:49:54 +0200

    poisson distribution 2 added

--- basic_probabilities.py
+++ basic_probabilities.py
@@ -59,6 +59,42 @@ def poison_dist():
     plt.show()
 
 
+def poison_dist_2():
+    import numpy as np
+    import matplotlib.pyplot as plt
+    from scipy.stats import poisson
+
+    # Parameters
+    lmbda = 71  # expectation
+
+    # Generate probabilities
+    x = np.arange(0, 200)
+    probs = poisson.pmf(x, lmbda)
+
+    # Find the number of meteorites until the probability remains less than 0.5%
+    threshold = 0.005
+    threshold_idx = np.where(probs < threshold)[0][0]
+
+    # Plot the probabilities
+    plt.figure(figsize=(10, 6))
+    plt.plot(x, probs, marker='o', linestyle='-', color='b')
+    plt.xlabel('Number of Meteorites')
+    plt.ylabel('Probability')
+    plt.title('Probability Distribution of Meteorites Falling on Ocean')
+    plt.axvline(x=threshold_idx, color='r', linestyle='--', label=f'Probability < {threshold}% for x > {threshold_idx}')
+    plt.legend()
+
+    # Calculate and plot expectation and median
+    expectation = poisson.mean(lmbda)
+    median = poisson.median(lmbda)
+    plt.axvline(x=expectation, color='g', linestyle='--', label=f'Expectation: {expectation:.2f}')
+    plt.axvline(x=median, color='purple', linestyle='--', label=f'Median: {median}')
+    plt.legend()
+
+    plt.grid(True)
+    plt.show()
+
+
 def negative_binomial_dist():
 
     # Parameters
@@ -137,6 +173,6 @@ def geometric_dist():
 
 if __name__ == '__main__':
     proportions_viz()
-    poison_dist()
+    poison_dist_2()
     negative_binomial_dist()
     geometric_dist()
