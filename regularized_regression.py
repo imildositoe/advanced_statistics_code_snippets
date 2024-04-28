6d96ab3acf57b974b65ed3031a6edadafbab9cdc
poisonshade <imildositoe@gmail.com>
2024-04-28 18:41:10 +0200

    code to print the rounded coefficients

--- regularized_regression.py
+++ regularized_regression.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import PolynomialFeatures
-from sklearn.linear_model import LinearRegression, Ridge
+from sklearn.linear_model import LinearRegression, Ridge, RidgeCV
 from sklearn.pipeline import Pipeline
 
 # Data given in the task
@@ -64,6 +64,10 @@ print(pipeline.steps[1][1].coef_)
 print('-------------------------------')
 print(ridge_pipeline.steps[1][1].coef_)
 
-[-2.45102755e+09, 1.54163931e+08, 1.56445499e+08, -8.25327605e+06, -2.13412020e+06, 1.22366752e+05, 4.34710995e+03, -6.37603975e+02, 5.01616160e+01, -4.96630319e+00, -1.30642987e-01]
+print('------------------------------- Rounded--------------------------------')
+a = [-2.45102755e+09, 1.54163931e+08, 1.56445499e+08, -8.25327605e+06, -2.13412020e+06, 1.22366752e+05, 4.34710995e+03, -6.37603975e+02, 5.01616160e+01, -4.96630319e+00, -1.30642987e-01]
+for aa in a:
+    print(np.round(aa, 2))
 
-[-2.28344677e+09, 1.50087545e+08, 1.48085753e+08, -8.13910869e+06, -2.00344222e+06, 1.21121431e+05, 3.47806954e+03, -6.32146360e+02, 5.27292402e+01, -4.97436077e+00, -1.33415648e-01]
+
+# b = [-2.28344677e+09, 1.50087545e+08, 1.48085753e+08, -8.13910869e+06, -2.00344222e+06, 1.21121431e+05, 3.47806954e+03, -6.32146360e+02, 5.27292402e+01, -4.97436077e+00, -1.33415648e-01]
