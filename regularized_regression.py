f55f35aca0eac4dee3e6c6dd6c5e06b90df496dc
poisonshade <imildositoe@gmail.com>
2024-04-28 19:18:01 +0200

    handwritten alpha determination

--- regularized_regression.py
+++ regularized_regression.py
@@ -39,16 +39,19 @@ plt.ylabel('y')
 plt.legend()
 plt.show()
 
+# Visualizing the coefficients of the OLS
+print(pipeline.steps[1][1].coef_)
+
 # --------------------------------------------------------
 
-ridge_pipeline = Pipeline([
+pipeline_ridge = Pipeline([
     ('polynomial', PolynomialFeatures(degree=11, include_bias=False)),
     ('ridge regression', Ridge(alpha=1))
 ])
-ridge_pipeline.fit(x, y)
+pipeline_ridge.fit(x, y)
 
 # Applying the prediction line using ridge
-y_values_ridge = ridge_pipeline.predict(x_values_plot)
+y_values_ridge = pipeline_ridge.predict(x_values_plot)
 
 # Visualizing the points jointly with the prediction line using ridge
 plt.figure(figsize=(10, 6))
@@ -59,15 +62,69 @@ plt.ylabel('y')
 plt.legend()
 plt.show()
 
-# Visualizing the coefficients of the OLS and after applying ridge
-print(pipeline.steps[1][1].coef_)
-print('-------------------------------')
-print(ridge_pipeline.steps[1][1].coef_)
+# Visualizing the coefficients of ridge
+print(pipeline_ridge.steps[1][1].coef_)
 
-print('------------------------------- Rounded--------------------------------')
-a = [-2.45102755e+09, 1.54163931e+08, 1.56445499e+08, -8.25327605e+06, -2.13412020e+06, 1.22366752e+05, 4.34710995e+03, -6.37603975e+02, 5.01616160e+01, -4.96630319e+00, -1.30642987e-01]
-for aa in a:
+
+def ridge_fit(train, predictors, target, alpha):
+    X = train[predictors].copy()
+    y = train[[target]].copy()
+
+    x_mean = X.mean()
+    x_std = X.std()
+
+    X = (X - x_mean) / x_std
+    X["intercept"] = 1
+    X = X[["intercept"] + predictors]
+
+    penalty = alpha * np.identity(X.shape[1])
+    penalty[0][0] = 0
+
+    B = np.linalg.inv(X.T @ X + penalty) @ X.T @ y
+    # B.index = ["intercept", "athletes", "events"]
+    return B, x_mean, x_std
+
+B, x_mean, x_std = ridge_fit(train, predictors, target, alpha)
+
+
+def ridge_predict(test, predictors, x_mean, x_std, B):
+    test_X = test[predictors]
+    test_X = (test_X - x_mean) / x_std
+    test_X["intercept"] = 1
+    test_X = test_X[["intercept"] + predictors]
+
+    predictions = test_X @ B
+    return predictions
+
+predictions = ridge_predict(test, predictors, x_mean, x_std, B)
+
+
+ridge = Ridge(alpha=alpha)
+ridge.fit(X[predictors], y)
+
+# sklearn_predictions = ridge.predict(test_X[predictors])
+# predictions - sklearn_predictions
+
+from sklearn.metrics import mean_absolute_error
+
+errors = []
+alphas = [10 ** i for i in range(-2, 4)]
+
+for alpha in alphas:
+    B, x_mean, x_std = ridge_fit(train, predictors, target, alpha)
+    predictions = ridge_predict(test, predictors, x_mean, x_std, B)
+
+    errors.append(mean_absolute_error(test[target], predictions))
+
+print(errors)
+
+
+
+# To remove
+print('-------Rounded---------')
+for aa in [-2.45102755e+09, 1.54163931e+08, 1.56445499e+08, -8.25327605e+06, -2.13412020e+06, 1.22366752e+05, 4.34710995e+03, -6.37603975e+02, 5.01616160e+01, -4.96630319e+00, -1.30642987e-01]:
     print(np.round(aa, 2))
-
-
-# b = [-2.28344677e+09, 1.50087545e+08, 1.48085753e+08, -8.13910869e+06, -2.00344222e+06, 1.21121431e+05, 3.47806954e+03, -6.32146360e+02, 5.27292402e+01, -4.97436077e+00, -1.33415648e-01]
+print('........')
+for aa in [-2.28344677e+09, 1.50087545e+08, 1.48085753e+08, -8.13910869e+06, -2.00344222e+06, 1.21121431e+05, 3.47806954e+03, -6.32146360e+02, 5.27292402e+01, -4.97436077e+00, -1.33415648e-01]:
+    print(np.round(aa, 2))
+print('-------Rounded---------')
