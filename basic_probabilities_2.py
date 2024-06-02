cfc62136315e1da7714ff5441fa37bbe9810e462
poisonshade <imildositoe@gmail.com>
2024-06-02 23:13:47 +0200

    independent and dependent values calculation merged

--- basic_probabilities_2.py
+++ basic_probabilities_2.py
@@ -14,15 +14,13 @@ def solve1():
     print("The probability of waiting between 2 and 4 hours is: ", quad(pdf, 2, 4)[0])
 
     # In order to calculate our probabilities, we will first create a range of our independent values
-    # We will create 500 values between 0 and 4 using linspace function
+    # We will create 500 values between 0 and 4 using linspace function following by determining the probabilities
     y_values = np.linspace(0, 4, 500)
-
-    # Calculate the PDF values
-    pdf_values = pdf(y_values)
+    prob_values = pdf(y_values)
 
     # Plot the PDF
     plt.figure(figsize=(10, 5))
-    plt.plot(y_values, pdf_values, label='PDF')
+    plt.plot(y_values, prob_values, label='PDF')
     plt.xlabel('Time (hours)')
     plt.ylabel('Probability Density')
     plt.title('Probability Density Function')
