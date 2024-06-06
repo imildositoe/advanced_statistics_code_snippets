8fe3628d09281648676fa355ff9af529eb76c180
poisonshade <imildositoe@gmail.com>
2024-06-06 18:17:42 +0200

    Lambda value calculated using the Wikipedia-Poisson approximation

--- basic_probabilities_1.py
+++ basic_probabilities_1.py
@@ -5,26 +5,28 @@ import matplotlib.pyplot as plt
 lambda_value = 71
 threshold_value = 0.005
 
+# First we generate values from 1 to 100
 # Calculate probabilities until they drop below 0.5%
-values_of_k = np.arange(0, 100)
+values_of_k = np.arange(1, 100)
 probs = poisson.pmf(values_of_k, lambda_value)
 
 # Determine the range of k where probabilities are above 0.5%
-valid_k_value = values_of_k[probs >= threshold_value]
+valid_k_values = values_of_k[probs >= threshold_value]
 
-# Calculate and plot the median
-cdf = poisson.cdf(values_of_k, lambda_value)
-median_value = np.where(cdf >= 0.5)[0][0]
+# Here we calculate the expectation value by attributing the given lambda value in the task
+# Then we compute the approximation value of median based on Wikipedia-Poisson formula
 expectation_value = lambda_value
+median_value = round(lambda_value + (1 / 3) - (1 / (50 * lambda_value)), 1)
 
+# Here we print in the console the calculated Expectation and Median
 print("Expectation value: ", expectation_value)
 print("Median value: ", median_value)
 
 # Plotting
 plt.figure(figsize=(10, 5))
-plt.bar(valid_k_value, probs[probs >= threshold_value], color='c', alpha=0.7, label='P(X = k)')
-plt.axvline(x=lambda_value, color='k', linestyle='--', label=f'Expectation: {lambda_value})')
-plt.axvline(x=median_value, color='r', linestyle='--', label=f'Median: {median_value})')
+plt.bar(valid_k_values, probs[probs >= threshold_value], color='c', alpha=0.7, label='P(X = k)')
+plt.axvline(x=lambda_value, color='k', linestyle='--', label=f'Expectation: {lambda_value}')
+plt.axvline(x=median_value, color='r', linestyle='--', label=f'Median: {median_value}')
 plt.xlabel('Meteorites number / k value')
 plt.ylabel('Probability value')
 plt.title('Meteorites falling on an ocean')
