11d98e60637ce78e81757b8bd5861c55c635a3af
poisonshade <imildositoe@gmail.com>
2024-06-06 21:43:42 +0200

    printing the valid k values and their probabilities

--- basic_probabilities_1.py
+++ basic_probabilities_1.py
@@ -10,16 +10,17 @@ threshold_value = 0.005
 expectation_value = lambda_value
 median_value = round(lambda_value + (1 / 3) - (1 / (50 * lambda_value)), 1)
 
-# First we generate the K values from 1 to 100 to be used to compute the probabilities until we find the range
+# First we generate the k values from 1 to 142 to be used to compute the probabilities until we find the range
 # of K values where the probability remains less than 0.5% (0.005) for any bigger number of meteorites
 values_of_k = np.arange(1, expectation_value * 2)
 probs = poisson.pmf(values_of_k, lambda_value)
 valid_k_values = values_of_k[probs >= threshold_value]
-print(values_of_k)
 
 # Here we print in the console the calculated Expectation and Median values
 print("Expectation value: ", expectation_value)
 print("Median value: ", median_value)
+print("Valid k values: ", valid_k_values)
+print("Valid probabilities", probs[probs >= threshold_value])
 
 # Here we plot the graph displaying the probabilities of meteorites falling on an ocean within the given conditions
 # We also display in the same graph the representation lines of the Expectation and the Median
@@ -27,7 +28,7 @@ plt.figure(figsize=(10, 5))
 plt.bar(valid_k_values, probs[probs >= threshold_value], color='c', alpha=0.7, label='P(X = k)')
 plt.axvline(x=lambda_value, color='k', linestyle='--', label=f'Expectation: {lambda_value}')
 plt.axvline(x=median_value, color='r', linestyle='--', label=f'Median: {median_value}')
-plt.xlabel('Meteorites number / k value')
+plt.xlabel('Meteorites number / k')
 plt.ylabel('Probability value')
 plt.title('Meteorites falling on an ocean in a given Year')
 plt.legend()
